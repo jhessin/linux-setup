42a6c48000c01b812bc7f8c9b4e299ebcaad4f1e
Jim Hessin <jhessin@gmail.com>
2023-04-12 22:50:03 -0600

    added nvim as a neovim command.

--- 01-setup-neovim.sh
+++ 01-setup-neovim.sh
@@ -4,6 +4,7 @@ sudo cp installers/nvim.appimage /usr/local/bin
 CUSTOM_NVIM_PATH=/usr/local/bin/nvim.appimage
 
 set -u
+sudo update-alternatives --install /usr/bin/nvim nvim "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
 sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
